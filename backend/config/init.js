ee51862692e0f239745d35bc42f84dca43d19dde
Aman Kumar <amn9955@gmail.com>
2020-05-05 17:03:31 +0530

    dockerize attempt: emergency commit

--- backend/config/init.js
+++ backend/config/init.js
@@ -1,17 +1,17 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-module.exports.initDb = function() {
-  mongoose.connect("mongodb://localhost:27017/bluffmaster", {
-    useNewUrlParser: true
+module.exports.initDb = function () {
+  mongoose.connect('mongodb://mongo:27017/bluffmaster', {
+    useNewUrlParser: true,
   });
   var db = mongoose.connection;
-  db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function() {
-    console.log("Connected");
-    Participants = require("../models/Participants");
-    Questions = require("../models/Questions");
-    Variables = require("../models/Variables");
-    Votes = require("../models/Votes");
-    Scores = require("../models/Scores");
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('Connected');
+    Participants = require('../models/Participants');
+    Questions = require('../models/Questions');
+    Variables = require('../models/Variables');
+    Votes = require('../models/Votes');
+    Scores = require('../models/Scores');
   });
 };
